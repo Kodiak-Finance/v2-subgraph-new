aa5f15a36ab7b8a23c01a0d23756cf5fcf6112c5
berastotle <berastotle@kodiak.finance>
2025-02-09 00:34:07 +0400

    fix hardcoded factory address

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -9,7 +9,7 @@ import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 import { TokenDefinition } from './tokenDefinition'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0x5e705e184d233ff2a7cb1553793464a9d0c3028f'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
